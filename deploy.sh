fa7506c7a9ba4719b297dbd89ef4387cdafc2309
sebastian-heinz <sebastian-heinz@users.noreply.github.com>
2020-05-13 13:28:34 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -142,7 +142,8 @@ Environment="DB_TYPE=sqlite"
 Environment="DB_USER="
 Environment="DB_PASS="
 Type=simple
-User=necromancy_server
+User=root
+Group=root
 ExecStart=$server_dir/Necromancy.Cli server start --service --max-packet-size=64 --b-list=3:0x2E64,0xFFFC,0xFFFD,0xFFFE,0xFFFF,3:0x6A72,3:0xE4AE,3:0x8D92,3:0x6B6A,0x8BB4,0x2470
 WorkingDirectory=$server_dir
 Restart=on-failure
